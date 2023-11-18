5f215815e89fcc2d0201bb78fe41dd6d72391be5
iwatanabee <issei.watanabe0708@gmail.com>
2023-11-18 13:48:34 +0900

    first page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+<<<<<<< HEAD
   def hello
+=======
+  def index
+>>>>>>> 6dc3534... first page
     render html: "hello, world"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   root "application#hello"
+=======
+  root "application#index"
+>>>>>>> 6dc3534... first page
 end
